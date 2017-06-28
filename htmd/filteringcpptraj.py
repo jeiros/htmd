086910a115853ff86be5cc2b4ff99296772cd10c
Juan Eiros <jeirosz@gmail.com>
2017-06-28 17:21:48 +0100

    forgot folder

--- htmd/filteringcpptraj.py
+++ htmd/filteringcpptraj.py
@@ -16,8 +16,8 @@ def simfiltercpptraj(sims, outfolder, cpptraj_file):
     for sim in sims:
         sim_name = sim.input.split('/')[1]
         logger.info('sim_name is {}'.format(sim_name))
-        if path.exists("{}/{}_filtered.nc".format(sim_name)):
-            logger.info('{}/{}_filtered.nc already exists, skipping filtering'.format(sim_name))
+        if path.exists("{}/{}_filtered.nc".format(outfolder, sim_name)):
+            logger.info('{}/{}_filtered.nc already exists, skipping filtering'.format(outfolder, sim_name))
         else:
             bashCommand = "cpptraj -p {} -y {} -i {} -x {}/{}_filtered.nc".format(sim.molfile,
                                                                                       sim.trajectory[0],
